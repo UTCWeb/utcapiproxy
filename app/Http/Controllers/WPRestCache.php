3055498bab6ceca43094579499779b0f71395a79
Bernardo Martinez <bernardo-martinez@utc.edu>
2023-03-17 16:47:49 -0400

    Added wordpress rest api cache to speed performance.

---
+++ app/Http/Controllers/WPRestCache.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Log;
+
+class WPRestCache extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+        $wpResponse = Cache::pull('wpResponsekey'.$id);
+        if($wpResponse === null) {
+            
+            Log::channel('stderr')->info('using the cache');
+            $wpResponse = Http::withHeaders([
+            ])->get("https://blog.utc.edu/news/wp-json/wp/v2/posts?_embed&per_page=".$id)->json();
+            Cache::put('wpResponsekey'.$id,$wpResponse, now()->addMinutes(10));
+            return $wpResponse;
+        } 
+        // return 'Using cached value';
+        return $wpResponse;
+
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
