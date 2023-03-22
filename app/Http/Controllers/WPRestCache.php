3f2b73048aad709f6597b499c450a562277e7e55
Bernardo Martinez <bernardo-martinez@utc.edu>
2023-03-22 15:34:59 -0400

    Working prototype version 2, Caches based on id, number of items capped at 12 and categories.

--- app/Http/Controllers/WPRestCache.php
+++ app/Http/Controllers/WPRestCache.php
@@ -44,14 +44,11 @@ class WPRestCache extends Controller
      * Display the specified resource.
      *
      * @param  string  $id
-     * @param  int     $numberofarticles
-     * @param  string  $categories
+     * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
     public function show($id)
     {
-
-
         // Checks if the response key json exist already or it fetches a new one.
         $wpResponse = null;
         if (Cache::has('wpResponsekey'.$id)) {
@@ -64,16 +61,17 @@ class WPRestCache extends Controller
         // ])->get("https://blog.utc.edu/".$id."/wp-json/wp/v2/posts?_embed&per_page=".$id)->json();
         // Cache::put('wpResponsekey'.$id, $wpResponse, now()->addMinutes(10));
         // return $wpResponse;
+
+        // $id = $request->input('id');
+        //  dd($id);
         $wpResponse = Http::withHeaders([])->get("https://blog.utc.edu/".$id."/wp-json/wp/v2/posts", [
             '_embed' => '1',
             'per_page' => '4',
             // 'categories' => 4,
         ])->json();
+
         Cache::put('wpResponsekey'.$id, $wpResponse, now()->addMinutes(10));
         return $wpResponse;
-
-
-
     }
 
     /**
@@ -97,6 +95,43 @@ class WPRestCache extends Controller
     public function update(Request $request, $id)
     {
         //
+        // Checks if the response key json exist already or it fetches a new one.
+        $wpResponse = $wpqueryparameters = $wpper_page = $wpcategories = null;
+        $wpper_page = $request->input('per_page') ?? '4';
+        $wpcategories = $request->input('categories');
+        $wpqueryparameters = implode(",", array_filter([$wpper_page, $wpcategories])) ;
+        if($wpper_page > 12 ? $wpper_page = 12 : $wpper_page);
+        if (Cache::has('wpResponsekey'.$id.$wpqueryparameters)) {
+            dd("cached content");
+            $wpResponse = Cache::pull('wpResponsekey'.$id.$wpqueryparameters);
+            return $wpResponse;
+        }
+        // ])->get("https://blog.utc.edu/".$id."/wp-json/wp/v2/posts?_embed&per_page=".$id)->json();
+        // $wpResponse = Http::withHeaders([
+        // ])->get("https://blog.utc.edu/".$id."/wp-json/wp/v2/posts?_embed&per_page=".$id)->json();
+        // Cache::put('wpResponsekey'.$id, $wpResponse, now()->addMinutes(10));
+        // return $wpResponse;
+
+        // $id = $request->input('id');
+        // dd($request->input('per_page'));
+        if ($wpcategories!= null) {
+            $wpResponse = Http::withHeaders([])->get("https://blog.utc.edu/".$id."/wp-json/wp/v2/posts", [
+                '_embed' => '1',
+                'per_page' => $wpper_page,
+                'categories' =>  $wpcategories,
+            ])->json();
+
+            Cache::put('wpResponsekey'.$id.$wpqueryparameters, $wpResponse, now()->addMinutes(60));
+            return $wpResponse;
+        }
+
+        $wpResponse = Http::withHeaders([])->get("https://blog.utc.edu/".$id."/wp-json/wp/v2/posts", [
+            '_embed' => '1',
+            'per_page' => $wpper_page,
+        ])->json();
+
+        Cache::put('wpResponsekey'.$id.$wpqueryparameters, $wpResponse, now()->addMinutes(60));
+        return $wpResponse;
     }
 
     /**
